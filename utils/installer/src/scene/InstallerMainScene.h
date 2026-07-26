c39c7dc8443cc79bf01c634428993212b7a37c42
SBKarr <sbkarr@stappler.org>
2026-07-27 05:04:46 +0800

    utils/installer initial

---
+++ utils/installer/src/scene/InstallerMainScene.h
@@ -0,0 +1,52 @@
+/**
+ Copyright (c) 2026 Xenolith Team <admin@xenolith.studio>
+
+ Permission is hereby granted, free of charge, to any person obtaining a copy
+ of this software and associated documentation files (the "Software"), to deal
+ in the Software without restriction, including without limitation the rights
+ to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ copies of the Software, and to permit persons to whom the Software is
+ furnished to do so, subject to the following conditions:
+
+ The above copyright notice and this permission notice shall be included in
+ all copies or substantial portions of the Software.
+
+ THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ THE SOFTWARE.
+ **/
+
+#ifndef UTILS_INSTALLER_SRC_SCENE_INSTALLERMAINSCENE_H_
+#define UTILS_INSTALLER_SRC_SCENE_INSTALLERMAINSCENE_H_
+
+#include "XL2dScene.h"
+
+namespace STAPPLER_VERSIONIZED stappler::xenolith::installer {
+
+// Используем базовую 2D-сцену в качестве основы
+class MainScene : public basic2d::Scene2d {
+public:
+	virtual ~MainScene() = default;
+
+	virtual bool init(NotNull<AppThread> app, NotNull<core::RenderServerChannel>,
+			const core::FrameConstraints &constraints) override;
+
+	virtual void handleContentSizeDirty() override;
+
+	virtual void handleEnter(Scene *) override;
+
+protected:
+	using Scene::init;
+
+	virtual void handlePresented(Director *) override;
+
+	virtual void buildQueueResources(QueueInfo &, core::Queue::Builder &builder) override;
+};
+
+} // namespace stappler::xenolith::installer
+
+#endif // UTILS_INSTALLER_SRC_SCENE_INSTALLERMAINSCENE_H_
